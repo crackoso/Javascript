fb4713c47bf45d292e2ee6c6b96b8976de03f762
Hibran Martinez <crack.oso@gmail.com>
2017-06-14 22:33:38 -0400

    working with variables

--- intro/script.js
+++ intro/script.js
@@ -1,3 +1,4 @@
+/* 
 var name = 'Hibran';
 console.log(name);
 
@@ -9,3 +10,28 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge);
+*/
+
+var name = 'Hibran';
+var age = 30;
+console.log(name + age);
+console.log(age + age);
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'Teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'Thirty six';
+job = 'Driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
