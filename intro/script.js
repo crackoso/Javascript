0d2a49fc2c8db9c309ccbc1df4fa8403f6ca2916
Hibran Martinez <crack.oso@gmail.com>
2017-06-27 19:55:27 -0400

    Coding Test 1

--- intro/script.js
+++ intro/script.js
@@ -1,29 +1,19 @@
-// boolean logic
+// Coding Test 1
 
-var age = 25;
-if(age < 20){
-    console.log('John is a teenager');
-} else if(age > 20 && age <30 ){
-    console.log('John is a young man');      
-}else {
-    console.log('John is a man');
+
+var jHeight = 181;
+var jAge = 32;
+
+var fHeight = 176;
+var fAge = 33;
+
+var john = jHeight + (jAge*5);
+var friend = fHeight + (fAge*5);
+
+if(john > friend){
+    console.log('John Wins: '+ john);
+} else if( friend > john){
+    console.log('Friend Wins: '+ friend);
+} else {
+    console.log('it was a tie');
 }
-
-
-var job = 'teacher'
-
-job = prompt('What does john do?');
-switch(job){
-    case 'teacher':
-        console.log('John teaches kids');
-        break;
-    case 'driver':
-        console.log('john drives a cab in Lisbon');
-        break;
-    case 'cop':
-        console.log('John helps fiht crime.');
-        break;
-    default:
-        console.log('John does something else');
-}
-
