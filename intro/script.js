8a2681aaeb5a33307af3f978d26a886026e09d21
Hibran Martinez <crack.oso@gmail.com>
2017-06-27 20:58:31 -0400

    Statements and expressions

--- intro/script.js
+++ intro/script.js
@@ -1,34 +1,18 @@
-// Functions
+// Statements and expressions
 
-function calculateAge(yearOfBirth) {
-    var age = 2017 - yearOfBirth;
-    //console.log(age);
-    return age;
+function someFun(par){
+    //code
 }
 
-//var ageMora = calculateAge(1990);
-//var ageEmma = calculateAge(1988);
-//var ageRamas = calculateAge(1993);
-//var ageWilly = calculateAge(1995);
-//var ageBerto = calculateAge(1997);
-
-
-function yearsUntilRetirement(name, year){
-    var age = calculateAge(year);
-    var retirement = 65 - age;
-    
-    if(retirement >= 0){
-        console.log(name + ' retires in ' + retirement + ' years');
-    } else{
-        console.log(name + ' is already retired');
-    }
-    return retirement;
+var someFun = function(par){
+    //code
 }
 
+//expression
+3+4;
+var x = 3;
 
-yearsUntilRetirement('Alejo', 1955);
-yearsUntilRetirement('Bertha', 1958);
-yearsUntilRetirement('Hibran', 1986);
-yearsUntilRetirement('Emma', 1988);
-yearsUntilRetirement('Mora', 1990);
-yearsUntilRetirement('Berto', 1997);
+//statements 
+if(x=== 5){
+    //do something
+}
