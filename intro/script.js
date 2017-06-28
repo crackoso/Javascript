085c09859531446cebec7f7be2094bbe63cf18d0
Hibran Martinez <crack.oso@gmail.com>
2017-06-27 20:54:03 -0400

    functions

--- intro/script.js
+++ intro/script.js
@@ -1,19 +1,34 @@
-// Coding Test 1
+// Functions
 
-
-var jHeight = 181;
-var jAge = 32;
-
-var fHeight = 176;
-var fAge = 33;
-
-var john = jHeight + (jAge*5);
-var friend = fHeight + (fAge*5);
-
-if(john > friend){
-    console.log('John Wins: '+ john);
-} else if( friend > john){
-    console.log('Friend Wins: '+ friend);
-} else {
-    console.log('it was a tie at ' + john);
+function calculateAge(yearOfBirth) {
+    var age = 2017 - yearOfBirth;
+    //console.log(age);
+    return age;
 }
+
+//var ageMora = calculateAge(1990);
+//var ageEmma = calculateAge(1988);
+//var ageRamas = calculateAge(1993);
+//var ageWilly = calculateAge(1995);
+//var ageBerto = calculateAge(1997);
+
+
+function yearsUntilRetirement(name, year){
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    
+    if(retirement >= 0){
+        console.log(name + ' retires in ' + retirement + ' years');
+    } else{
+        console.log(name + ' is already retired');
+    }
+    return retirement;
+}
+
+
+yearsUntilRetirement('Alejo', 1955);
+yearsUntilRetirement('Bertha', 1958);
+yearsUntilRetirement('Hibran', 1986);
+yearsUntilRetirement('Emma', 1988);
+yearsUntilRetirement('Mora', 1990);
+yearsUntilRetirement('Berto', 1997);
